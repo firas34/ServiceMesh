062f8208127a19079b397929cd22cce377bb39ad
MERLIER <chemli@eurecom.fr>
2021-01-10 10:33:40 +0100

    update

--- blockGenerator.py
+++ blockGenerator.py
@@ -1,16 +1,12 @@
 import os, requests
 ch = str(input("Please give the concerned host IP...\n"))
-target = str(input("Please give the concerned application...\n"))
-l = str(input("Please give the IP address that you want to block...\n"))
-List = '["'+l+'" ]'
-first = "apiVersion: security.istio.io/v1beta1\nkind: AuthorizationPolicy\nmetadata:\n  name: ingress-policy\n  namespace: default\nspec:\n  selector:\n    matchLabels:\n"
-target = "      app: " + target + "\n"
-second = "  action: DENY\n  rules:\n  - from:\n    - source:\n"
-List = "        ipBlocks: " + List + "\n"
-final = first + target + second + List
-f = open("rule.yaml", "w")
-f.write(final)
-f.close()
-# os.system('kubectl apply -f rule.yaml') when you are in the same machine
-print(requests.get(ch+'/applyRule').text)
+#target = str(input("Please give the concerned application...\n"))
+#l = str(input("Please give the IP address that you want to block...\n"))
 
+#JSONFILE= { "appName": target, "ExceptionIp": l , "rule": "DENY" }
+
+
+# os.system('kubectl apply -f rule.yaml') when you are in the same machine
+#print(requests.post(ch+'/addRule' , json=JSONFILE).text)
+#ad1b2639-7e8f-40e4-a41d-27e3bffc531d
+print(requests.delete(ch+'/deleteRule/ad1b2639-7e8f-40e4-a41d-27e3bffc531d' ).text)
--- ruleApi.py
+++ ruleApi.py
@@ -1,12 +1,49 @@
-from flask import Flask
+from flask import Flask , request
 import os
 app = Flask(__name__)
+#global ruleId = 1111
+import uuid
 
-@app.route('/applyRule')
-def applyRule():
+def generateRuleFile(JSONFILE, ruleId):
+    target = JSONFILE["appName"]
+    l = JSONFILE["ExceptionIp"]
+    rule = JSONFILE["rule"].upper()
+    List = '["'+l+'" ]'
+    first = "apiVersion: security.istio.io/v1beta1\nkind: AuthorizationPolicy\nmetadata:\n  name: "+ruleId +"\n  namespace: default\nspec:\n  selector:\n    matchLabels:\n"
+    target = "      app: " + target + "\n"
+    second = "  action: "+ rule +"\n  rules:\n  - from:\n    - source:\n"
+    List = "        ipBlocks: " + List + "\n"
+    final = first + target + second + List
+    f = open("rule.yaml", "w")
+    f.write(final)
+    f.close()
+
+@app.route('/addRule', methods=["POST"])
+def addRule():
+    JSONFILE = request.get_json()
+    ruleId  = str(uuid.uuid4()) 
+    generateRuleFile(JSONFILE, ruleId)
+    # save in db
     stream = os.popen('kubectl apply -f rule.yaml')
+    return { "response": stream.read() , "ruleId": ruleId }
+
+@app.route('/deleteRule/<ruleId>', methods=["DELETE"])
+def deleteRule(ruleId):
+    stream = os.popen('kubectl delete AuthorizationPolicy '+ ruleId)
     return stream.read()
 
+
+@app.route('/updateRule/<ruleId>', methods=["PUT"])
+def updateRule(ruleId):
+    JSONFILE = request.get_json()
+    generateRuleFile(JSONFILE, ruleId)
+    # save in db
+    stream = os.popen('kubectl apply -f rule.yaml')
+    return { "response": stream.read() , "ruleId": ruleId }
+
+@app.route('/showRule/<ruleId>', methods=["GET"])
+def showRule(ruleId):
+    return "next version maybe!"
 
 if __name__ == "__main__":
 	app.run(port=9090)
